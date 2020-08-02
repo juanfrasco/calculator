720b53b94f6bd4e7cfcf8499ac6febb03dc2efab
juanfrasco <you@example.com>
2020-08-02 13:03:55 +0200

    Add Spring Boot skeleton

---
+++ src/test/java/com/juanfrasco/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanfrasco.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
