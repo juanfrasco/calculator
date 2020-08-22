c4e5777f5b66ea971bab6f4c6693c3e74c449dbc
juanfrasco <you@example.com>
2020-08-22 21:47:16 +0200

    Add docker push stage to Jenkinsfile 17

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-CALCULATOR_IP=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" calculator)
-test $(curl $CALCULATOR_IP:8765/sum?a=1\&b=2) -eq 3 && echo "Acceptance Test OK" || echo "Acceptance Test NOK"
+test $(curl localhost:8765/sum?a=1\&b=2) -eq 3
