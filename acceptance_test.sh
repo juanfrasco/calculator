0d79a12ee0b55bd421be63cb56f98468a9f9540b
juanfrasco <you@example.com>
2020-08-22 21:24:14 +0200

    edit Jenkinsfile 10

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-test $(curl -s localhost:8765/sum?a=1\&b=2) -eq 3 && echo "Acceptance Test OK" || echo "Acceptance Test NOK"
+test $(curl localhost:8765/sum?a=1\&b=2) -eq 3 && echo "Acceptance Test OK" || echo "Acceptance Test NOK"
