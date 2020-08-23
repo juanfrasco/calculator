9d6fc6bbdd4e1808ec2bd6b835e4add25cb506ae
juanfrasco <you@example.com>
2020-08-23 20:54:59 +0200

    acceptance_test edition 3

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-test $(curl localhost:8765/sum?a=1\&b=2) -eq 3
+test $(curl $(/sbin/ip route|awk '/default/ { print  $3}'):8765/sum?a=1\&b=2) -eq 3 && echo 'ACCEPT TEST OK' || echo 'ACCEPT TEST NOK'
